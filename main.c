26334590cda1d869f07a0915c04b4beb78bcf52d
tusharK007 <51186687+tusharK007@users.noreply.github.com>
2020-08-14 11:01:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,19 @@ Initial Function()
 {
   //Sample Code
 }
+Initial Security()
+{
+ //Sample Code
+}
 
 feature1()
 {
   //Sample Code for Feature 1
+}
+
+feature2()
+{
+  //Sample Code for Feature 2
 }
 
 
